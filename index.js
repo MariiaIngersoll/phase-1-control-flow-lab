330d440a2ea06a11b8d9d08a752f9fa8c562484d
+ <+>
2023-02-22 23:10:57 -0500

    done

--- index.js
+++ index.js
@@ -1,11 +1,42 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
-}
+function scuberGreetingForFeet(number){
+  if (number<=400) {
+    return 'This one is on me!'
+  }
+  else if (number > 400 && number < 2000) {
+    return 'That will be twenty bucks.'
+  }
 
-function ternaryCheckCity(){
-  // Write your code here!
+  else if  (number > 2000 && number <2500) {
+    return 'I will gladly take your thirty bucks.'
+  }
+  else if (number > 2500) {
+    return 'No can do.'
+  }
 }
+scuberGreetingForFeet(199);
+scuberGreetingForFeet(1500);
+scuberGreetingForFeet(2001);
+scuberGreetingForFeet(2501);
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+
+
+function ternaryCheckCity(city) {
+  return (city === 'NYC' ? "Ok, sounds good." : "No go.");
 }
+ternaryCheckCity('NYC');
+
+function switchOnCharmFromTip(tip){
+  switch (tip) {
+    case 'generous':
+    return 'Thank you so much.';
+    break;
+  case 'not as generous':
+  return 'Thank you.'
+  case 'thanks for everything' :
+  return 'Bye.'
+  break;
+  } 
+}
+switchOnCharmFromTip(generous);
+switchOnCharmFromTip(notasgenerous);
+switchOnCharmFromTip('thanks for everything');
